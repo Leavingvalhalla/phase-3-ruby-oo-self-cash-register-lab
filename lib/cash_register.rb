4910156312fa990f7ce7300231c7ac89d93c7afc
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-09 12:39:15 -0700

    initial commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+    attr_accessor :total, :discount
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+    end
 
+    def add_item(title, price, quantity=1)
+        self.total = price * quantity
+    end
+
+    def apply_discount
+        puts self.discount
+        parsed_discount = ((100 - self.discount) / 100)
+        puts parsed_discount
+        self.total *= parsed_discount
+    end
+
+
+
+end
+
+cash = CashRegister.new(20)
+cash.add_item('thing', 50)
+cash.apply_discount
+# puts cash.total
